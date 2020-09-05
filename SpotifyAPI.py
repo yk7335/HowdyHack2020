133917074d1a1fdce1d804211e59876451544611
Yash Kalyani <yashkalyani456@gmail.com>
2020-09-05 18:12:22 -0500

    find song titles

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,3 +1,4 @@
-artist = "Taylor Swift"
-Song = "Delicate"
-keyword = 
+import lyricsgenius
+genius = lyricsgenius.Genius("lhTCJEt_2R38qM9LC4b_TrPjurZL0Rwfljyp6J1C0IoLMG3le-MN0qbI94XSB70y")
+artist = genius.search_artist("Drake", max_songs=10, sort="title")
+print(artist.songs)
