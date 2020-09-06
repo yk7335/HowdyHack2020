1f7e42834b389a1f25a6e72d2dc98ff3deb3bba2
Yash Kalyani <yashkalyani456@gmail.com>
2020-09-06 11:12:17 -0500

    Made SongScoreList

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -3,20 +3,15 @@ import random
 from MainScript import* 
 
 songList = []
-songArtist = []
+songArtist = ["Drake", "Drake"]
 genius = lyricsgenius.Genius("lhTCJEt_2R38qM9LC4b_TrPjurZL0Rwfljyp6J1C0IoLMG3le-MN0qbI94XSB70y")
 songScoreList = []
 songsStringList = []
+TopThreeSongs = []
 
-def get_Artists():
-    while(len(songArtist) < 3):
-        artistname = str(input("Enter Artist: "))
-        songArtist.append(artistname)
-
-
-def MakeSongList():
+def MakeSongList(songArtist):
     for i in songArtist:
-        artist = genius.search_artist(i, max_songs=15, sort="title")
+        artist = genius.search_artist(i, max_songs=50, sort="title")
         temp = random.randint(0,11)
         temp2 = 0
         while(temp2 < 10):
@@ -36,15 +31,15 @@ def GetSongToString(SongTitle, SongArtist):
 
 
 def GetSongScore():
-    get_Artists()
-    MakeSongList()
-    for i in range len(songsStringList)-1:
+    MakeSongList(songArtist)
+    
+    for i in range(49):
         songsStringList[i] = GetSongToString(str(songList[i].title), str(songList[i].artist))
     
-    for i in range len(songScoreList)-1:
-        songScoreList[i] =  ParseEntry(songsStringList[i])
-    
 
-
-# the scores are stored in song score list at the same index of thier lyrics and song title / artist
-# to figure out which 
+    for i in range(49):
+        songScoreList[i] =  ParseEntry(songsStringList[i])    
+            
+# the scores are in a song score list at the same index of thier lyrics and song title / artist
+# to figure out which three we have to index through the list and see which has the highest score get
+# index and see what song / artist is on that index in SongList
