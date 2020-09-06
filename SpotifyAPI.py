ed3bc23c2b2f5b289aca1da0260df6d8e73fce17
Yash Kalyani <yashkalyani456@gmail.com>
2020-09-05 21:59:37 -0500

    Turned the API into functions

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -2,29 +2,39 @@ import lyricsgenius
 import random
 from MainScript import* 
 
-songArtist = ["Drake"]
-songList=[]
-
+songList = []
+songArtist = []
 genius = lyricsgenius.Genius("lhTCJEt_2R38qM9LC4b_TrPjurZL0Rwfljyp6J1C0IoLMG3le-MN0qbI94XSB70y")
 
-for i in songArtist:
-    artist = genius.search_artist(i, max_songs=15, sort="title")
-    temp = random.randint(0,11)
-    temp2 = 0
-    while(temp2 < 10):
-        if artist.songs[random.randint(0,11)] in songList:
-            temp = random.randint(0,11) 
-        else:
-            songList.append(artist.songs[temp])
-            temp = random.randint(0,11)
-            temp2 += 1
-
-print(songList[0].artist)
-#song = genius.search_song(str(songList[0]), str(songList[0][1]))
-
-# test = song.lyrics.split(" ")
-# for i in test:o
-#     print(i, " "0)
+def get_Artists():
+    while(len(songArtist) < 3):
+        artistname = str(input("Enter Artist: "))
+        songArtist.append(artistname)
 
 
+def MakeSongList():
+    for i in songArtist:
+        artist = genius.search_artist(i, max_songs=15, sort="title")
+        temp = random.randint(0,11)
+        temp2 = 0
+        while(temp2 < 10):
+            if artist.songs[random.randint(0,11)] in songList:
+                temp = random.randint(0,11) 
+            else:
+                songList.append(artist.songs[temp])
+                temp = random.randint(0,11)
+                temp2 += 1
 
+get_Artists()
+#song = genius.search_song("Marvins Room", str(songList[0].artist))
+MakeSongList()
+
+def GetSongToString(SongTitle, SongArtist):
+    song = genius.search_song(SongTitle, SongArtist)
+    SongString = "".join(song.lyrics)
+    return SongString
+
+for i in range(5):
+    temp3 = GetSongToString(str(songList[i].title), str(songList[i].artist))
+    print(temp3)
+    print("----------------------------------------------------------------")
