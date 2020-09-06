1d26bf5cf059110782e4839cb0f9d0ddf0d2777d
Yash Kalyani <yashkalyani456@gmail.com>
2020-09-06 10:48:22 -0500

    added getsongscore

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -5,6 +5,8 @@ from MainScript import*
 songList = []
 songArtist = []
 genius = lyricsgenius.Genius("lhTCJEt_2R38qM9LC4b_TrPjurZL0Rwfljyp6J1C0IoLMG3le-MN0qbI94XSB70y")
+songScoreList = []
+songsStringList = []
 
 def get_Artists():
     while(len(songArtist) < 3):
@@ -25,16 +27,24 @@ def MakeSongList():
                 temp = random.randint(0,11)
                 temp2 += 1
 
-get_Artists()
-#song = genius.search_song("Marvins Room", str(songList[0].artist))
-MakeSongList()
 
 def GetSongToString(SongTitle, SongArtist):
     song = genius.search_song(SongTitle, SongArtist)
     SongString = "".join(song.lyrics)
     return SongString
 
-for i in range(5):
-    temp3 = GetSongToString(str(songList[i].title), str(songList[i].artist))
-    print(temp3)
-    print("----------------------------------------------------------------")
+
+
+def GetSongScore():
+    get_Artists()
+    MakeSongList()
+    for i in range len(songList)-1:
+        songsStringList[i] = GetSongToString(str(songList[i].title), str(songList[i].artist))
+    
+    for i in range len(songScoreList)-1:
+        songScoreList[i] =  ParseEntry(songsStringList[i])
+    
+
+
+# the scores are stored in song score list at the same index of thier lyrics and song title / artist
+# to figure out which 
